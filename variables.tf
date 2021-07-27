7e892646537f3002bc122a26a622daea9557b8c4
Marco Mercado <marcore.dmg@gmail.com>
2021-07-27 17:01:50 -0500

    lesson 36

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ext_port" {
+  type = number
+}
+
+variable "int_port" {
+  type = number
+  default = 1880
+  validation {
+    condition = var.int_port == 1880
+    error_message = "The internal port must be 1880."
+  }
+}
+
+variable "container_count" {
+  type = number
+  default = 1
+}
