0823c5bc37666fb3146a164413244f743da66c3e
Marco Mercado <marcore.dmg@gmail.com>
2021-07-29 16:39:58 -0500

    windows adjustments

--- main.tf
+++ main.tf
@@ -7,7 +7,9 @@ terraform {
   }
 }
 
-provider "docker" {}
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
 
 resource "docker_image" "nodered_image" {
   # name = var.image[var.env]
